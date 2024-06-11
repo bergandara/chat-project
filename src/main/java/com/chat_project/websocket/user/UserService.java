ad0b7de28f872a030b500b74c90652c417f901e8
bergandara <gandaraber@gmail.com>
2024-06-12 00:14:22 +0100

    Create UserService.java

---
+++ src/main/java/com/chat_project/websocket/user/UserService.java
@@ -0,0 +1,24 @@
+package com.chat_project.websocket.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    public void saveUser(User user){
+
+    }
+
+    public void disconnect(User user){
+
+    }
+
+    public List<User> findConnectedUsers(){
+
+        return null;
+    }
+}
