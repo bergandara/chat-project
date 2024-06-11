29a9947c396600c23280c158a68c2428cffc81ba
bergandara <gandaraber@gmail.com>
2024-06-12 00:19:31 +0100

    Methods bodies implemented

--- src/main/java/com/chat_project/websocket/user/UserService.java
+++ src/main/java/com/chat_project/websocket/user/UserService.java
@@ -12,15 +12,21 @@ public class UserService {
     private final UserRepository repository;
 
     public void saveUser(User user){
-
+        user.setStatus(Status.ONLINE);
+        repository.save(user);
     }
 
     public void disconnect(User user){
-
+        var storedUser = repository.findById(user.getNickName())
+                .orElse(null);
+        if(storedUser != null){
+            storedUser.setStatus(Status.OFFLINE);
+            repository.save(storedUser);
+        }
     }
 
     public List<User> findConnectedUsers(){
 
-        return null;
+        return repository.findAllByStatus(Status.ONLINE);
     }
 }
