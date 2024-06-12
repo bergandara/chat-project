ba136137487ed4aebc072d2b155833eb622f9720
bergandara <gandaraber@gmail.com>
2024-06-12 14:55:47 +0100

    Multiple functions added to main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -39,7 +39,55 @@ function onConnected(){
     stompClinet.subscribe(`/user/${nickname}/queue/messages`, onMessageReceived); //Connected user to his own queue, everytime he gets a new message he'll be notified
     stompClinet.subscribe(`/user/public`, onMessageReceived);
 
-    //
+    //register the connected user
+    stompClient.send('app/user.addUser',
+        {},
+        JSON.stringify({nickName: nickname, fullName: fullname, status: 'ONLINE'}) //stringify transforms any object to JSON.
+     );
+    //Find and display the connected users
+    findAndDisplayConnectedUsers().then();
+
+}
+
+async function findAndDisplayConnectedUsers(){
+    const connectedUserResponse = await fetch('/users');
+    let connnectedUsers = await connectedUserResponse.json();
+    connectedUsers = connectedUsers.filter(user => user.nickName !== nickname);
+    const connectedUsersList = document.getElementById('connectedUsers');
+    connectedUsersList.innerHTML = '';
+
+    connectedUsersList.forEach(user => {
+        appendUserElement(user, connectedUsersList);
+        if (connectedUsers.index(user) < connectedUsers.length - 1) {
+            //add a separator
+            const separator = document.createElement('li');
+            separator.classList.add('separator');
+            connectedUsersList.appendChild(separator);
+        }
+    });
+}
+
+function appendUserElement(user, connectedUserList){
+    const listItem = document.createElement('li');
+    listItem.classList.add('user-item');
+    listItem.id = user.nickName;
+
+    const userImage = document.createElement('img');
+    userImage.src = '../img/user_icon.png';
+    userImage.alt = user.fullName;
+
+    const usernameSpan = document.createElement('span');
+    usernameSpan.textContent = user.fullName;
+
+    const receivedMsgs = document.createElement('span');
+    receivedMsgs.textContent = '0';
+    receivedMsgs.classList.add('nbr-msg', 'hidden');
+
+    listItem.appendChild(userImage);
+    listItem.appendChild(usernameSpan);
+    listItem.appendChild(receivedMsgs);
+
+    connectedUsersList.appendChild(listItem);
 }
 
 function onError(){
